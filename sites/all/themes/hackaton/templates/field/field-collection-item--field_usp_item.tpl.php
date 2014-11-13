f90f4aa6f3412f60cb60c05e3da8745cb4c1571f
Andrey Posudevsky <aposudevsky@gmail.com>
2014-11-14 00:29:48 +0100

    usp items block

---
+++ sites/all/themes/hackaton/templates/field/field-collection-item--field_usp_item.tpl.php
@@ -0,0 +1,9 @@
+<div class="span4">
+    <div class="service clearfix">
+        <i class="<?php echo render($content['field_usp_icon']) ?>"></i>
+        <div class="desc">
+            <h4><?php echo render($content['field_usp_title']) ?></h4>
+            <p><?php echo render($content['field_usp_intro']) ?></p>
+        </div>
+    </div>
+</div>

6b44433bb75bfe58f65780323cf313cc0cba380e
Andrey Posudevsky <aposudevsky@gmail.com>
2014-11-14 15:16:46 +0100

    text page advanced

--- sites/all/themes/hackaton/templates/node/node--text_page_advanced.tpl.php
+++ sites/all/themes/hackaton/templates/node/node--text_page_advanced.tpl.php
@@ -2,6 +2,7 @@
     <div class="row-fluid">
         <?php if (!empty($content['field_text_page_snippets'])) : ?>
             <div class="span9">
+                <?php echo render($content['field_text_page_advanced_body']); ?>
                 <?php echo render($content['field_text_page_content_items']); ?>
             </div>
             <div class="span3">
@@ -9,6 +10,7 @@
             </div>
         <?php else : ?>
             <div class="span12">
+                <?php echo render($content['field_text_page_advanced_body']); ?>
                 <?php echo render($content['field_text_page_content_items']); ?>
             </div>
         <?php endif; ?>
